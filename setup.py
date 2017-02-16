3ac5f964876a0c96afcb42728294190ef40f6513
Amit Jaiswal <amitjaiswal@Amits-MacBook-Pro.local>
2017-02-16 15:49:07 +0530

    fix setup

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ for datadir in ["bin", "res"]:
 print datafiles
 
 setup(
-    name = "Analytics-DailyUpdate",
-    version = "3.0",
-    description = "Daily hive and redshift query execution",
+    name = "Analytics-LookerData",
+    version = "1.0",
+    description = "Runs looker queries to warm up cache",
     author = "Rishi",
     packages = find_packages(),
     include_package_data=True,
@@ -23,19 +23,13 @@ setup(
         'requests == 2.9.1',
         'psycopg2 == 2.6.1',
         'Analytics-CommonUtilities >= 3.2.1',
-        'redis == 2.10.5',
-        'redlock == 1.2.0',
         'python-dateutil == 2.6.0'
     ],
     dependency_links = [
         "https://gdeploy:a13e8c8e9da9c0f06643146abd78ab1261934808@github.com/hike/Analytics-CommonUtilities/archive/master.zip#egg=Analytics-CommonUtilities-3.2.1"
     ],
-    scripts=['daily_update/scripts/redshift_daily_upload.py',
-             'daily_update/scripts/redshift_backfill_days.py',
-             'daily_update/scripts/redshift_backfill_history.py',
-             'daily_update/scripts/redshift_check_x_percent_complete.py',
-             'daily_update/scripts/redshift_update_hdr_parameters_1.py',
-             'daily_update/scripts/schedule_hive_to_redshift.py'],
+    scripts=['daily_queries/scripts/looker_daily_query.py',
+             ],
     data_files=datafiles,
     zip_safe=False
 )
