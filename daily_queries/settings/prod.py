50ff6cc89b921d2320f53ae3d55d506d44b917c5
Amit Jaiswal <amitjaiswal@Amits-MacBook-Pro.local>
2017-02-17 06:24:02 +0530

    adding 140,141 dashboard

--- daily_queries/settings/prod.py
+++ daily_queries/settings/prod.py
@@ -1,4 +1,6 @@
 from daily_queries.settings.common import *
 NAGIOS_QUERY_FILE_FORMAT = "/var/log/nagios_check_files/looker_query_failed-%s"
 _whitelisted_queries = { "117" : "",
-                         "118" : ""}
+                         "118" : "",
+                         "140" : "",
+                         "141" : ""}
